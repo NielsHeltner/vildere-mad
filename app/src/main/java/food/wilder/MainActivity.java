ecec96f4f76cbe9840dc4af1bfedf4f9df120e20
Niels Heltner <nhelt15@student.sdu.dk>
2018-10-27 01:41:55 +0200

    added butterknife bindings

--- app/src/main/java/food/wilder/MainActivity.java
+++ app/src/main/java/food/wilder/MainActivity.java
@@ -2,12 +2,28 @@ package food.wilder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.loggedInAsTxt)
+    private TextView loggedInAsTxt;
+    @BindView(R.id.profileBtn)
+    private Button profileBtn;
+    @BindView(R.id.leaderboardsBtn)
+    private Button leaderboardsBtn;
+    @BindView(R.id.mapBtn)
+    private Button mapBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+    
 }
