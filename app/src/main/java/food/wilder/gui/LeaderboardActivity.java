2c05b2191d36659f911e88ef31fa36dc66701c65
Niclas Mølby <niclas@molby.net>
2018-11-07 18:05:51 +0100

    Added a http request for the users

--- app/src/main/java/food/wilder/gui/LeaderboardActivity.java
+++ app/src/main/java/food/wilder/gui/LeaderboardActivity.java
@@ -4,6 +4,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,13 +49,51 @@ public class LeaderboardActivity extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        mAdapter = new LeaderboardRecyclerViewAdapter(testData());
-        mRecyclerView.setAdapter(mAdapter);
+//        mAdapter = new LeaderboardRecyclerViewAdapter(testData());
+//        mRecyclerView.setAdapter(mAdapter);
+
+        populateRecyclerView();
     }
 
-    private List<UserData> getUsers() {
+    private void populateRecyclerView() {
+        String url = getResources().getString(R.string.get_users_end_point);
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+
+                    @Override
+                    public void onResponse(JSONArray response) {
+                       //createUserDataList(response);
+                       mAdapter = new LeaderboardRecyclerViewAdapter(createUserDataList(response));
+                       mRecyclerView.setAdapter(mAdapter);
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("GET_REQUEST", error.getMessage());
+                    }
+                });
+        queue.add(jsonArrayRequest);
+    }
+
+    private List<UserData> createUserDataList(JSONArray jsonDataArray) {
         List<UserData> usersList = new ArrayList<>();
 
+        for(int i = 0; i < jsonDataArray.length(); i++) {
+            try {
+                JSONObject jsonData = jsonDataArray.getJSONObject(i);
+
+                String name = jsonData.getString("username");
+                int level = jsonData.getInt("level");
+
+                usersList.add(new UserData().setName(name).setLevel(level));
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
         return usersList;
     }
 
